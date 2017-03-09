b9d524f030ce6256073f0c461e916e010f6793fd
ronniewang <ro_wsy@163.com>
2017-03-09 18:44:37 +0800

    some content in step 4

---
+++ FoodTracker/RatingControll.swift
@@ -0,0 +1,21 @@
+//
+//  RatingControll.swift
+//  FoodTracker
+//
+//  Created by Ronnie Wang on 17/3/9.
+//  Copyright © 2017年 Ronnie Wang. All rights reserved.
+//
+
+import UIKit
+
+class RatingControll: UIStackView {
+
+    //MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
